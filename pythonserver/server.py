26c2a914a5e35ae17e32aca4999c69cda9481eb8
amin_manna <amin10@meet.mit.edu>
2015-03-10 05:23:56 -0400

    Simple web server for 6.S198

---
+++ pythonserver/server.py
@@ -0,0 +1,22 @@
+from os import curdir, sep
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class S198_Handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print self.path
+        f = open(curdir + sep + "index.html")
+        self.send_response(200)
+        self.send_header('Content-type',	'text/html')
+        self.end_headers()
+        self.wfile.write(f.read())
+        self.wfile.write("<p id='username'>"+self.path[1:]+"</p>")
+        self.wfile.write("\n</HTML>")
+        f.close()
+        return
+try:
+    server = HTTPServer(('', 8000), S198_Handler)
+    print 'Started Server on port 8000'
+    server.serve_forever()
+except KeyboardInterrupt:
+    server.socket.close()
